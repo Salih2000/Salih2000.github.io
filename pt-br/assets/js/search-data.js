10cc024523929b4b489ca64521dce770d5e65408
Salih2000 <58088794+Salih2000@users.noreply.github.com>
2025-04-10 06:02:29 +0000

    Deploying to gh-pages from @ Salih2000/Salih2000.github.io@70b6653e6881d83f72db1986ec20d8d0789142b2 🚀

--- pt-br/assets/js/search-data.js
+++ pt-br/assets/js/search-data.js
@@ -519,14 +519,21 @@ ninja.data = [{
         title: 'Enviar um email',
         section: 'Redes sociais',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%73%61%6C%69%68%61%62%64%75%6C%6C%61%68%6B%69%6C%69%63@%68%6F%74%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Redes sociais',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/Salih2000", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Redes sociais',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/salih-abdullah-kilic", "_blank");
         },
       },{
         id: 'social-rss',
@@ -534,20 +541,6 @@ ninja.data = [{
         section: 'Redes sociais',
         handler: () => {
           window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Redes sociais',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Redes sociais',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
           id: 'lang-en-us',
