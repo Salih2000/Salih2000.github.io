630857e5b67b3479e8f6547a6d0a8c3aa65f46ae
Salih2000 <58088794+Salih2000@users.noreply.github.com>
2026-09-20 08:51:04 +0000

    Deploying to gh-pages from @ Salih2000/Salih2000.github.io@301a276882165734260e2d87f62ecca608a7cc7c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -15,27 +15,27 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation menu",
     handler: () => {
       window.location.href = "/";
     },
   },{id: "nav-blog",
-          title: "blog",
+          title: "Blog",
           description: "a simple whitespace theme for academics",
           section: "Navigation menu",
           handler: () => {
             window.location.href = "/blog/";
           },
         },{id: "nav-repositories",
-          title: "repositories",
+          title: "Repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
           section: "Navigation menu",
           handler: () => {
             window.location.href = "/repositories/";
           },
         },{id: "nav-cv",
-          title: "cv",
+          title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation menu",
           handler: () => {
